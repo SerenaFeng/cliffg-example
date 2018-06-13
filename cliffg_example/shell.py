7bad8e70cf663ebf776f564f69868bf5009928b9
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-06-13 10:03:08 +0800

    all rest support
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- cliffg_example/shell.py
+++ cliffg_example/shell.py
@@ -1,7 +1,10 @@
+
 import sys
 
 from cliff import app
 from cliff import commandmanager
+
+from cliffg_example.utils import rest
 
 
 class CliffgexampleShell(app.App):
@@ -14,8 +17,24 @@ class CliffgexampleShell(app.App):
             deferred_help=True,
         )
 
+    def build_option_parser(self, description, version, argparse_kwargs=None):
+        self.LOG.debug('build_option_parser')
+        parser = super(CliffgexampleShell, self).build_option_parser(
+            description,
+            version,
+            argparse_kwargs)
+#        parser.add_argument('-u',
+#                            type=str,
+#                            help='Username for authentication')
+#        parser.add_argument('-p',
+#                            type=str,
+#                            help='Password for authentication')
+        return parser
+
     def initialize_app(self, argv):
         self.LOG.debug('initialize_app')
+        self.rest_manager = rest.RestManager(self.options)
+        
 
     def prepare_to_run_command(self, cmd):
         self.LOG.debug('prepare_to_run_command %s', cmd.__class__.__name__)
